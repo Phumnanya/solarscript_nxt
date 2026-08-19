53b4dbb06c45a636aff9935d17378465043b8ba4
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2026-08-19 07:19:28 +0100

    Added Analytics

--- app/layout.js
+++ app/layout.js
@@ -25,6 +25,7 @@ export default function RootLayout({ children }) {
     </head>
       <body style={{fontFamily: "Arial, Helvetica, sans-serif"}}>
         {children}
+  <Analytics />
       </body>
     </html>
   );
